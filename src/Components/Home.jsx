bd7d63f2527c65bd66cf8b4b0bd38160f87d8fb4
SrLuuL <srpudao54@gmail.com>
2022-02-12 05:45:59 -0300

    Site React.js

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+import Logo from './Logo';
+import LinkPanel from './LinkPanel';
+
+function Home() {
+    return (
+        <div class="main">
+        <Logo />
+        <LinkPanel />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Components/Logo.jsx
@@ -0,0 +1,12 @@
+import './Logo.css'
+
+export  default function Logo() {
+    return (
+        <div class='logo'>
+            <p> &lt; </p>
+            <p id='center-letter'> L </p>
+            <p> &gt; </p>
+        </div>
+    )
+}
+
